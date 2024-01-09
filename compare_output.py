191a85124e6f3154830a87f422bbf05534261fe6
PawKanarek <paw.kanarek@gmail.com>
2024-01-09 13:15:51 +0000

    add compare image

--- compare_output.py
+++ compare_output.py
@@ -2,6 +2,7 @@ import os
 import re
 import matplotlib.pyplot as plt
 from PIL import Image
+from mpl_toolkits.axes_grid1 import ImageGrid
 
 OUTPUT_DIR = "output/sdxl-base-flax/spraix_sdxl_best_96_16"
 
@@ -9,28 +10,28 @@ OUTPUT_DIR = "output/sdxl-base-flax/spraix_sdxl_best_96_16"
 def sort_by_number(a):
     # extract the last numbers & and sort
     pattern = re.compile(r"(\d+)(?=\D*$)")
-    return sorted(a, key=lambda x: int(pattern.search(x).group(1)))
+    return reversed(sorted(a, key=lambda x: int(pattern.search(x).group(1))))
 
 
 if __name__ == "__main__":
     imgs = [f for f in os.listdir(OUTPUT_DIR) if ".png" in f]
     dirs = [d for d in os.listdir(OUTPUT_DIR) if "_epoch_" in d]
-    dirs = dirs[:2]
-    # wspaces = [0.3 if i % 8 == 0 else 0 for i, _ in enumerate(imgs)]
-    # hspaces = [0.05 for i, _ in enumerate(imgs)]
-    fig, axs = plt.subplots(len(dirs), len(imgs))
-    # plot all epochs
-    for i, dir in enumerate(sort_by_number(dirs)):
-        imgs = os.listdir(os.path.join(OUTPUT_DIR, dir))
+    scale = 64
+    fig = plt.figure(figsize=(2 * scale, 1 * scale))
+    grid = ImageGrid(fig, 111, nrows_ncols=(len(dirs), len(imgs)), axes_pad=0.1)
 
+    for row, dir in enumerate(sort_by_number(dirs)):
+        imgs = os.listdir(os.path.join(OUTPUT_DIR, dir))
         for j, img_path in enumerate(sort_by_number(imgs)):
-            row = len(dirs) - i - 1
-            ax = axs[row, j]
+            index = (row * len(imgs)) + j
+            ax = grid[index]
+            print(f"{img_path=}, {index=}, {row=}")
             img = Image.open(os.path.join(OUTPUT_DIR, dir, img_path))
             ax.imshow(img)
             ax.get_xaxis().set_visible(False)
             ax.get_yaxis().set_visible(False)
-            if j % 8 == 0:
+
+            if row == 0 and j % 8 == 0:
                 # remove hash, img number and .png from img path
                 ax.set_title(img_path[:-43].replace("_", " "), loc="left")
 
@@ -40,6 +41,8 @@ if __name__ == "__main__":
                 ax.get_yaxis().set_visible(True)
                 ax.set_yticklabels([])
 
-    fig.subplots_adjust(left=0.01, top=1, right=1, bottom=0, wspace=0.05, hspace=0)
+    fig.subplots_adjust(
+        left=0.005, top=0.995, right=0.995, bottom=0.005, wspace=0.005, hspace=0.005
+    )
     fig.savefig("cmp.png")
     print("done")

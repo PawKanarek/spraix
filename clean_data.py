ecd2434571c0e378d90240681e56d29209c84d79
Pawel Kanarek <paw.kanarek@gmail.com>
2024-01-31 15:50:40 +0100

    cleanup 9 frames version

--- clean_data.py
+++ clean_data.py
@@ -1,10 +1,16 @@
 import os
 import json
 
-FOLDERS = ["train_data_512", "train_data_1024", "train_data_1024_16", "train_data_1024_best", "train_data_1024_best_96"]
+FOLDERS = [
+    "train_data_512",
+    "train_data_1024",
+    "train_data_1024_16",
+    "train_data_1024_best",
+    "train_data_1024_best_96",
+    "train_data_1024_9frames",
+]
 
 if __name__ == "__main__":
-    
     for folder in FOLDERS:
         all_images = os.listdir(os.path.join(folder, "images"))
         with open(os.path.join(folder, "metadata.jsonl"), "r+") as f:
@@ -13,8 +19,8 @@ if __name__ == "__main__":
                 if any(os.path.join("images", image) in line for image in all_images):
                     lines_to_save.append(line)
                 else:
-                    print(f'removing line: {line}')
-            
+                    print(f"removing line: {line}")
+
             f.seek(0)
             f.truncate()
             f.writelines(lines_to_save)
